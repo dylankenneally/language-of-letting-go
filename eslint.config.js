cbc3c351205d7c108f2903acb574300a0c924ad7
Dylan Kenneally <github@dylankenneally.com>
2026-04-24 20:24:18 +1000

    Added linting to the project

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+import js from "@eslint/js";
+import globals from "globals";
+import markdown from "@eslint/markdown";
+import css from "@eslint/css";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
+  { files: ["**/*.md"], plugins: { markdown }, language: "markdown/commonmark", extends: ["markdown/recommended"] },
+  { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
+  { ignores: [
+    "build/",
+    "node_modules/",
+    "wip.md",
+  ] },
+]);
