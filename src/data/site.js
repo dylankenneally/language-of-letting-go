0baf26d5674aed9042f51216991163772dcdb923
Dylan Kenneally <github@dylankenneally.com>
2026-04-24 18:30:49 +1000

    Updated HTML semantics for a11y and SEO - adjacent navigation section

--- src/data/site.js
+++ src/data/site.js
@@ -1,9 +1,11 @@
 import { getPathPrefix, getSiteUrl } from "../../site-config.js";
 
 export default {
+  // todo: translate these strings (en / en-US here)
   title: "The Language of Letting Go",
   description: "Daily Meditations on Codependency",
   notFound: "Meditation not found",
+
   pathPrefix: getPathPrefix(),
   url: getSiteUrl(),
 };
